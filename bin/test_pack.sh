bfb78f32d2531e7bce0cad20fd41fcb72004c0a2
John L. Gardenghi <john.gardenghi@unb.br>
2019-09-21 15:55:50 -0300

    Removing private files

--- bin/test_pack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {1000,2000,4000,6000,8000,10000}
-do
-    time=$(date)
-    echo $time "- Running packing problem with N =" $i
-    ./pack -N $i -s arp -p 2 -pl 2 > $i"_arp_p2.out"
-done
